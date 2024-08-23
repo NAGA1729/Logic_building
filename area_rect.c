923cde4716712ebea57a0b210de274fd713c17f4
Naga Vinay <140480951+NAGA1729@users.noreply.github.com>
2024-08-23 09:35:07 +0530

    simple programs

---
+++ area_rect.c
@@ -0,0 +1,11 @@
+//Area of a rectangle(lxb)
+#include<stdio.h>
+int main(){
+	int l,b;
+	printf("Enter the Length of rectangle:");
+	scanf("%d",&l);
+	printf("Enter the Breadth of rectangle:");
+	scanf("%d",&b);
+	printf("The Area of rectangle with length %d and with breadth %d is: %d\n",l,b,(l*b));
+	return 0;
+}
---
+++ asci_valof_char.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	char ch;
+	printf("Enter the Character for ASCII value:");
+	scanf("%c",&ch);
+	printf("The ASCII value of char %c is: %d\n",ch,ch);
+	return 0;
+}
